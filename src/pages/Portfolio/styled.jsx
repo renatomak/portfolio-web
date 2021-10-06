ed70634264de1be80c8c153ce820fda556cbbed6
renatomak <renato.mark.silva@gmail.com>
2021-10-06 15:16:47 -0300

    refactoring style portfolio

--- src/pages/Portfolio/styled.jsx
+++ src/pages/Portfolio/styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 90vh;
   justify-content: space-between;
   animation-name: content;
   animation-duration: 2s;
